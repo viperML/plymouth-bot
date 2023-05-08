f235c5db0f551b0dc68bdc040308d898418cf9ef
Fernando Ayats <ayatsfer@gmail.com>
2023-05-08 17:29:59 +0000

    rewrite project

--- flake.nix
+++ flake.nix
@@ -2,73 +2,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-filter.url = "github:numtide/nix-filter";
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
-    fenix,
+    flake-parts,
     ...
   }:
-    inputs.flake-parts.lib.mkFlake {inherit self;} {
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "aarch64-linux"
         "x86_64-linux"
       ];
-
-      flake.overlays.default = final: prev: let
-        inherit (nixpkgs) lib;
-        variant = "beta";
-        buildSystem = final.stdenv.buildPlatform.system;
-        targetConfig = "${final.stdenv.targetPlatform.uname.processor}-unknown-linux-musl";
-      in {
-        _toolchain = with fenix.packages.${buildSystem};
-          combine [
-            (fenix.packages.${buildSystem}.${variant}.withComponents [
-              "rustc"
-              "cargo"
-            ])
-            targets.${targetConfig}.${variant}.rust-std
-          ];
-
-        plymouth-bot =
-          (final.makeRustPlatform {
-            cargo = final._toolchain;
-            rustc = final._toolchain;
-          })
-          .buildRustPackage {
-            src = inputs.nix-filter.lib {
-              root = ./.;
-              include = [
-                (inputs.nix-filter.lib.inDirectory "src")
-                "Cargo.toml"
-                "Cargo.lock"
-              ];
-            };
-            name = "plymouth-bot";
-            cargoLock.lockFile = ./Cargo.lock;
-            # target = targetConfig;
-            # CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-          };
-
-        _toolchain_dev = with fenix.packages.${buildSystem};
-          combine [
-            (fenix.packages.${buildSystem}.${variant}.withComponents [
-              "rustc"
-              "cargo"
-              "rust-src"
-              "clippy"
-              "rustfmt"
-              "rust-analyzer"
-            ])
-            targets.${targetConfig}.${variant}.rust-std
-          ];
-      };
 
       perSystem = {
         system,
@@ -76,30 +23,20 @@
         config,
         ...
       }: {
-        _module.args.pkgs = import nixpkgs {
-          inherit system;
-          overlays = [self.overlays.default];
-        };
-
-        packages = {
-          # default = pkgs.plymouth-bot;
-          # static = pkgs.pkgsCross.aarch64-multiplatform-musl.plymouth-bot;
-          # default = (pkgs.makeRustPlatform {
-          #   cargo = pkgs._toolchain;
-          #   rustc = pkgs._toolchain;
-          # }).buildRustPackage {
-          # };
-        };
-
         devShells.default = with pkgs;
           mkShell {
-            RUST_SRC_PATH = "${pkgs._toolchain_dev}/lib/rustlib/src/rust/library";
+            # Shell with CC
+            # name = "nh-dev";
+            RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
             packages = [
-              pkgs._toolchain_dev
+              cargo
+              rustc
+              rustfmt
+              clippy
+              rust-analyzer-unwrapped
             ];
+            RUST_BACKTRACE = "full";
           };
-
-        legacyPackages = pkgs;
       };
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,179 +1,50 @@
-pub mod danbooru;
-pub mod saucenao;
-
-use anyhow::{anyhow, bail, Context};
-use danbooru::DanbooruId;
-use log::{error, info, warn};
-use saucenao::SaucenaoClient;
-
 use clap::Parser;
+use color_eyre::Result;
+use redacted_debug::RedactedDebug;
+use std::path::PathBuf;
+use tracing::trace;
+use tracing_subscriber::prelude::*;
 
-use std::fmt::Debug;
-use std::fs::{self};
-use std::path::Path;
-
-use crate::danbooru::DanbooruClient;
-
-const CAG_INPUT: &str = "CAG_INPUT";
-
-const EXTENSIONS: &[&str] = &["png", "jpg", "jpeg"];
-const MINIMUM_SIMILARITY: f32 = 70.0;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[derive(RedactedDebug, Clone, Parser)]
 struct Args {
-    #[arg(short, long, default_value_t = 1)]
-    /// Number of files to process
-    items: usize,
+    /// Don't perform any action
     #[arg(short = 'n', long)]
-    /// Don't move any file
     dry: bool,
+    /// Base path for images
+    #[arg(short, long, env = "PWD")]
+    path: PathBuf,
+    /// Danbooru username
+    #[arg(env = "DANBOORU_USERNAME", long)]
+    #[redacted]
+    danbooru_username: String,
+    /// Danbooru apikey
+    #[arg(env = "DANBOORU_APIKEY", long)]
+    #[redacted]
+    danbooru_apikey: String,
 }
 
-fn main() {
-    match real_main() {
-        Ok(_) => {
-            std::process::exit(0);
-        }
-        Err(error) => {
-            for chain_elem in error.chain().rev() {
-                error!("{}", chain_elem);
-            }
-            std::process::exit(1);
-        }
-    }
-}
+fn main() -> Result<()> {
+    color_eyre::install()?;
 
-fn real_main() -> anyhow::Result<()> {
-    setup_logging()?;
+    let layer_filter = tracing_subscriber::EnvFilter::from_default_env()
+        .add_directive("info".parse()?)
+        .add_directive("plymouth_bot=trace".parse()?);
+
+    let layer_fmt = tracing_subscriber::fmt::layer()
+        .with_writer(std::io::stderr)
+        // .without_time()
+        .with_line_number(true)
+        .compact();
+
+    tracing_subscriber::registry()
+        .with(layer_filter)
+        .with(layer_fmt)
+        .init();
+
     let args = Args::parse();
 
-    let files = fs::read_dir(CAG_INPUT)
-        .context("Failed to open CAG_INPUT")?
-        .map(|entry| entry.map(|direntry| direntry.path()))
-        .collect::<Result<Vec<_>, _>>()
-        .context("Failed to get CAG_INPUT items")?;
-
-    let files = files
-        .iter()
-        .filter(|elem| match elem.extension().and_then(|o_s| o_s.to_str()) {
-            None => false,
-            Some(ext) => EXTENSIONS.contains(&ext),
-        })
-        .collect::<Vec<_>>();
-
-    let saucenao_apikey =
-        std::env::var("SAUCENAO_APIKEY").context("Failed to get saucenao credentials")?;
-    let mut saucer = SaucenaoClient::new(&saucenao_apikey);
-    info!("param MINIMUM_SIMILARITY: {MINIMUM_SIMILARITY}");
-
-    let danbooru_client = DanbooruClient::new(
-        &std::env::var("DANBOORU_USERNAME").context("Failed to get Danbooru credentials")?,
-        &std::env::var("DANBOORU_APIKEY").context("Failed to get Danbooru credentials")?,
-    );
-
-    for &path in files.iter().take(args.items) {
-        let file_name = path
-            .file_name()
-            .ok_or_else(|| anyhow!("Failed to get failname for {:?}", path))?;
-
-        info!("Processing {:?}", path);
-
-        if saucer.short_remaining == 0 {
-            warn!("Short limit reached, sleeping 30 seconds");
-            let duration = std::time::Duration::from_secs(31);
-            std::thread::sleep(duration);
-        }
-
-        match saucer.tag_image(path, MINIMUM_SIMILARITY) {
-            Ok(id) => {
-                info!("Adding to favourites!");
-                dispatch(path, Some((&id, &danbooru_client)), args.dry)?;
-            }
-            Err(error) => {
-                if error.chain().any(|cause| {
-                    matches!(cause.downcast_ref(), Some(saucenao::SauceError::NoMatch))
-                }) {
-                    dispatch(path, None, args.dry)?;
-                } else {
-                    bail!(error);
-                };
-            }
-        }
-    }
+    trace!("Tracing setup");
+    trace!(?args);
 
     Ok(())
-}
-
-fn setup_logging() -> anyhow::Result<()> {
-    let colors = fern::colors::ColoredLevelConfig::new()
-        .info(fern::colors::Color::Green)
-        .debug(fern::colors::Color::BrightBlue);
-
-    let loglevel = if cfg!(debug_assertions) {
-        log::LevelFilter::Debug
-    } else {
-        log::LevelFilter::Info
-    };
-
-    let logfile = std::fs::OpenOptions::new()
-        .write(true)
-        .create(true)
-        .append(false)
-        .truncate(true)
-        .open("plymouth-bot.log")?;
-
-    fern::Dispatch::new()
-        .level(loglevel)
-        .chain(std::io::stdout())
-        .chain(logfile)
-        .format(move |out, message, record| {
-            out.finish(std::format_args!(
-                "[{}][{}] {}",
-                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-                colors.color(record.level()),
-                message
-            ))
-        })
-        .apply()?;
-
-    Ok(())
-}
-
-fn dispatch<P: AsRef<Path>>(
-    path: P,
-    target: Option<(&DanbooruId, &DanbooruClient)>,
-    dry: bool,
-) -> anyhow::Result<()> {
-    match target {
-        None => {
-            if !dry {
-                info!("Nosaucing file");
-                organize_file(path, false)?;
-            }
-        }
-        Some((id, client)) => {
-            client.fav_post(id)?;
-            if !dry {
-                info!("Saucing file");
-                organize_file(path, true)?;
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn organize_file<P: AsRef<Path>>(path: P, sauced: bool) -> Result<(), std::io::Error> {
-    let file_name = path.as_ref().file_name().unwrap();
-
-    let new_path = if sauced {
-        Path::new("CAG_SAUCE").join(file_name)
-    } else {
-        Path::new("CAG_NOSAUCE").join(file_name)
-    };
-
-    info!("{:?} -> {:?}", path.as_ref(), new_path);
-
-    std::fs::rename(path.as_ref(), new_path)
 }
